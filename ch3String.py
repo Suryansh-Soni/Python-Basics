4b3b9dab13cb1af5081c5eaca524165aec18c82b
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-03 18:02:14 +0530

    chapter 3 strings,f-strings and its methods

---
+++ ch3String.py
@@ -0,0 +1,27 @@
+str="string"
+str2=" s"
+# print(str + str2)
+# print(len(str))
+# print(str[1])
+# print(str[::-1])
+
+# .len() ,.upper() ,.lower() ,.split() ,.join() ,.replace() ,
+# .find() ,.index() ,.count() ,.strip() ,.startswith() ,.endswith() ,etc.
+# print(str.replace('s','S'))
+
+# f-srting
+# area=3.14*5*5
+# print(f"the area of circle is {area}") 
+
+# escape sequences
+# \n,\t,\\,\' ,\" etc.
+# print("hello\nworld")
+# print("hello\tworld")
+# print("hello\\world")
+# print("hello\'world")
+# print("hello\"world")
+
+# concatination +,repeatition *,membership in,identity is,not in,not is, etc.
+print("hello"*2)
+print("h" in "hello")
+print("z" not in "hello")
