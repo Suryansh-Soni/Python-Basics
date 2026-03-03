de87f862ffe434157796e0719d56ef67fade79ca
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-03 18:00:21 +0530

    chapter 1 : input fro user

---
+++ ch1.py
@@ -0,0 +1,20 @@
+# s="09994"
+# reverse=s[::-1]
+# print(reverse)
+
+# rev_str = "".join(reversed("7005")) 
+# print(rev_str)
+
+# from user input
+# name=input("enter your name:")
+# print(name[::-1])
+# Reversing using joined iterator
+
+# age=input("enter your age:")
+# print(age)
+
+dim=input("enter the diameter:")
+area=3.14*(int(dim)/2)**2
+print(area)
+
+
