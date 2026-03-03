8f973567304a5ac7c80faed1c48cd6372d13d975
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-03 18:03:18 +0530

    chapter 5 dictionary and sets

---
+++ ch5_Dictionary.py
@@ -0,0 +1,28 @@
+# dictionary: it is unordered and mutable,
+# no duplicate keys 
+# methods in dictionary: .keys() , .values() , .items() , .get() , .update() , etc.
+
+
+# student={"name":"surynsh","age":20,"course":"datascience"}
+# print(student["name"])
+# student["age"]=21
+# print(student["age"])
+# student["favSub"]="math"
+# print(student["favSub"])
+# student.keys()
+# print(student.values())
+# print(student.update({"age":21}))
+
+
+# Set:unordered unique items, no indexing 
+# methods in set: .add() ,.remove() ,.discard() ,.union() ,.intersection() ,.difference() ,etc.
+# s={1,2,3,4,5}
+# s.add(6)
+# print(s)
+# s.remove(3)
+# print(s) 
+
+# conversion form list to set 
+list=[1,2,3,4,5]
+set1=set(list)
+print(set1)
