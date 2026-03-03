d381a6a33ed97f4f7db5eb8e45d9d3723ef0bec7
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-03 18:03:04 +0530

    chapter 4:conditional statement , lists and tuples

---
+++ ch4_ifelse,list.py
@@ -0,0 +1,33 @@
+
+# a=3
+# b=4
+
+# if(a<b):
+#     print(f"{a} is less than {b}")
+# elif(a>b):
+#     print(f"{a} is greater than {b} ")
+# else:
+#     print(f"{a} is equal to {b}")
+
+# list:vlues are mutable
+# a=[1,2,3,4]
+# a[2]=7
+# print(a[2])
+# print(a[1:4])
+# print(max(a))
+# a.append("suryansh")
+# a.remove(4)
+# a.insert(2,7)
+# print(a.pop(0))
+# print(a)
+
+# tupple:immutable
+b=(1,2,3,3,4)
+print(b[2])
+print(b.index(3))
+print(b.count(3))
+print(max(b))
+
+  
+# a=(1,)
+# print(len(a))
