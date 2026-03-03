f5732be0d04ae19e3d714574a03a0e2a9805b965
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-03 18:01:16 +0530

    chapter 2:operator and datatype

---
+++ ch2.py
@@ -0,0 +1,31 @@
+# data type
+# a="Hello World"
+# print(type(a))
+
+a=input("enter a number:")
+print(type(a))
+
+# operators
+# a=10
+# b=20
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+# # comparision operators
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+# # logical operators
+# print(a>5 and b>15)
+# print(a>5 or b>25)  
+# print(not(5))
+
+# # assignment operators
+# a=40//20
+# print(a)    
