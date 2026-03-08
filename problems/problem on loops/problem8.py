6519a1c7c10990c95f500ef0af18b85b52c96d51
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-08 17:41:23 +0530

    project 8: Exponential backoff

---
+++ problems/problem on loops/problem8.py
@@ -0,0 +1,10 @@
+# Exponential backoff double the wait time for 5 tries and after taht it stops
+import time
+wait_time=1
+max_retries=5
+attempts=0
+while(attempts<max_retries):
+    print("Attempts",attempts+1,"-waittime,wai",wait_time)
+    time.sleep(wait_time)
+    wait_time*=2
+    attempts+=1
