83a64ed3ab74638faf0b659395ba0e6d67246ad5
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-08 16:50:17 +0530

    problem 3:according to weather suggest a activity .

---
+++ problems/problems of conditional statements/problem3.py
@@ -0,0 +1,8 @@
+# accordinfg to the weather give appropreate activity .
+weather=input("Enter the weather: ")
+weather=weather.lower()
+if(weather=="sunny"):
+    print("play outdoors.")
+if(weather== "rainy"):
+    print("play indoors.")
+
