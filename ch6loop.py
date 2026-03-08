a6d3747e0e5fc59386a447e517d620d90e51559a
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-08 18:28:29 +0530

    iterables more depth

--- ch6loop.py
+++ ch6loop.py
@@ -12,3 +12,9 @@ for num in range(1, 101):
         else:
             print(num)
             break
+
+
+# iteration tool: for ,comprehensions
+# iterable objects: lists tupple files etc.
+# open   file as f , here f is iterable but list =[1,2,3] list is not 
+# __nest__(), i=iter(list) here i is iterable then i.__next__() giev op 1 i.__next__()->2 and so on  next(I) same 
