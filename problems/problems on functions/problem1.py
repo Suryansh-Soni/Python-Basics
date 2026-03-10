feac483c19efe33b0a4e75908331d73be2ff1cd2
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-10 18:16:59 +0530

    Problem 1: making a simple calculator using function

---
+++ problems/problems on functions/problem1.py
@@ -0,0 +1,12 @@
+# Problem 1: making a simple calculator using function 
+def square(n):
+    return n*n
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mult(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+print(square(2))
