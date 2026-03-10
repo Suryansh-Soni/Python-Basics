c63c1e3366c04ee4d5af143216b6eed0c378d18b
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-10 18:20:10 +0530

    #  Problem 2: take radius and return area and circumference

---
+++ problems/problems on functions/problem2.py
@@ -0,0 +1,8 @@
+#  Problem 2: take radius and return area and circumference 
+
+import math ;
+def circle_stats(r):
+    area=int(math.pi*r*r)
+    circum=int(math.pi*2*r)
+    return area,circum
+print(circle_stats(5)) 
