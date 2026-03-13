b7f377730ab6300ae6a1c7b758a27c1aa780aad3
Suryansh Soni <suryanshsoni92@gmail.com>
2026-03-13 18:40:46 +0530

    debuging function call

--- decorators.py
+++ decorators.py
@@ -16,3 +16,20 @@ def example(n):
     time.sleep(n)
 
 example(2)
+
+# debuging function call
+
+def debug(fn):
+    def wrapper(*args,**kwargs):
+        args_value=', '.join(str(arg) for arg in args) # join is used to get itteratable 
+        kwargs_value=' ,'.join(f"{k}={v}" for k,v in kwargs.items())
+        print(f"calling: {fn.__name__} with args:  {args_value} and kwargs: {kwargs_value}")
+        return fn(*args,**kwargs)
+
+    return wrapper
+
+@debug
+def greet(name,greetig="hello"):
+    print(f"{greetig}, {name}")
+
+greet("chai" ,greetig="namaste")
